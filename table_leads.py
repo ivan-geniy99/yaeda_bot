d773f095267112488a52c7cd0cf69b5572e0b115
ivan-geniy99 <ivandugar163@gmail.com>
2026-01-26 20:33:39 +0400

    правлю время логов на самарское

--- table_leads.py
+++ table_leads.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from google_client import get_google_client
 
 scopes = [
@@ -9,11 +9,13 @@ scopes = [
 client = get_google_client(scopes)
 sheet = client.open("ready_on_onboarding").sheet1
 
-
 def save_lead(data: dict):
+    # Время с +4 часа
+    current_time = datetime.utcnow() + timedelta(hours=4)
+
     # Приводим все значения к строкам, если данных нет — пустая строка
     row = [
-        datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        current_time.strftime("%Y-%m-%d %H:%M:%S"),
         data.get("user_id", ""),
         data.get("username", ""),
         data.get("age", ""),
